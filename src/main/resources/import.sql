fce5928e8343a7687ba514ffca4f6d4d35ef33f2
brian cruz <brian.lancruz@gmail.com>
2025-10-22 14:50:32 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,10 +10,10 @@ INSERT INTO artista (id, nome, genero_musical, biografia) VALUES (3, 'Drake', 'H
 INSERT INTO artista (id, nome, genero_musical, biografia) VALUES (4, 'Lady Gaga', 'Pop', 'Cantora, compositora e atriz americana');
 
 -- Inserir eventos
-INSERT INTO evento (id, nome, descricao, data_evento, local, capacidade_maxima, preco_ingresso, status) VALUES (1, 'Festival de Verao', 'O maior festival de verao do pais com os melhores artistas', '2024-12-15', 'Praia de Copacabana', 50000, 250.0, 'DISPONIVEL');
-INSERT INTO evento (id, nome, descricao, data_evento, local, capacidade_maxima, preco_ingresso, status) VALUES (2, 'Rock in Rio', 'O famoso festival de rock que acontece no Brasil', '2024-09-15', 'Parque Olimpico', 100000, 350.0, 'DISPONIVEL');
-INSERT INTO evento (id, nome, descricao, data_evento, local, capacidade_maxima, preco_ingresso, status) VALUES (3, 'Lollapalooza', 'Festival de musica alternativa que reune diversos generos', '2024-03-25', 'Autodromo de Interlagos', 80000, 300.0, 'DISPONIVEL');
-INSERT INTO evento (id, nome, descricao, data_evento, local, capacidade_maxima, preco_ingresso, status) VALUES (4, 'Tomorrowland', 'Festival de musica eletronica mais famoso do mundo', '2024-07-20', 'Boom, Belgica', 60000, 500.0, 'DISPONIVEL');
+INSERT INTO evento (id, nome, descricao, data_evento, local, capacidade_maxima, preco_ingresso, status) VALUES (1, 'Festival de Verao', 'O maior festival de verao do pais com os melhores artistas', '2026-12-15', 'Praia de Copacabana', 50000, 250.0, 'DISPONIVEL');
+INSERT INTO evento (id, nome, descricao, data_evento, local, capacidade_maxima, preco_ingresso, status) VALUES (2, 'Rock in Rio', 'O famoso festival de rock que acontece no Brasil', '2026-09-15', 'Parque Olimpico', 100000, 350.0, 'DISPONIVEL');
+INSERT INTO evento (id, nome, descricao, data_evento, local, capacidade_maxima, preco_ingresso, status) VALUES (3, 'Lollapalooza', 'Festival de musica alternativa que reune diversos generos', '2026-03-25', 'Autodromo de Interlagos', 80000, 300.0, 'DISPONIVEL');
+INSERT INTO evento (id, nome, descricao, data_evento, local, capacidade_maxima, preco_ingresso, status) VALUES (4, 'Tomorrowland', 'Festival de musica eletronica mais famoso do mundo', '2026-07-20', 'Boom, Belgica', 60000, 500.0, 'DISPONIVEL');
 
 -- Inserir relação entre eventos e artistas
 INSERT INTO evento_artista (evento_id, artista_id) VALUES (1, 1);
@@ -32,4 +32,4 @@ INSERT INTO ingresso (id, nome_comprador, email_comprador, data_compra, quantida
 INSERT INTO ingresso (id, nome_comprador, email_comprador, data_compra, quantidade, preco_total, status, evento_id) VALUES (4, 'Ana Oliveira', 'ana@email.com', '2023-11-13 16:20:00', 2, 600.0, 'RESERVADO', 3);
 
 -- Inserir API Keys de exemplo
-INSERT INTO api_key (id, chave, usuario_id, data_criacao, data_expiracao, status) VALUES (1, 'demo_key_123456', 1, '2024-01-01 10:00:00', '2024-12-31 23:59:59', 'ATIVA');
+INSERT INTO api_key (id, chave, usuario_id, data_criacao, data_expiracao, status) VALUES (1, 'demo_key_123456', 1, '2024-01-01 10:00:00', '2026-12-31 23:59:59', 'ATIVA');
